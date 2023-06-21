1875ad3b5600651c2e00fd6e2c11a56b916f97df
Nestor Júnior <nestorjunior@MacBook-Air-de-Nestor.local>
2023-06-21 11:31:25 -0300

    Update: register user

---
+++ src/components/AlertMessage.jsx
@@ -0,0 +1,11 @@
+const AlertMessage = () => {
+    return (
+        <>
+            <div className="alert alert-danger" role="alert">
+                <h3>Campos precisam ser preenchidos</h3>
+            </div>
+        </>
+    )
+}
+
+export default AlertMessage
--- src/components/FormSignup.jsx
+++ src/components/FormSignup.jsx
@@ -1,28 +1,59 @@
 import { useState } from "react";
+import {auth} from '../utils/FirebaseConfig'
+import {createUserWithEmailAndPassword} from 'firebase/auth'
 
 const FormSignup = () => {
 
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const [repeatPassword, setRepeatPassword] = useState();
+    const [email, setEmail] = useState()
+    const [password, setPassword] = useState()
+    // const [repeatPassword, setRepeatPassword] = useState()
 
     const handleEmail = (e) => {
-        setEmail(e.target.value);
+        setEmail(e.target.value)
     }
 
     const handlePassword = (e) => {
-        setPassword(e.target.value);
+        setPassword(e.target.value)
     }
 
-    const handleRepeatPassword = (e) => {
-        setRepeatPassword(e.target.value);
+    // const handleRepeatPassword = (e) => {
+    //     setRepeatPassword(e.target.value)
+    // }
+
+    const handleValidatePassword = () => {
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((res) => {
+            console.log(res.user)
+            })
+        .catch(err => (err.message))
+        console.log('Usuário cadastrado com sucesso!')  
+
+        // if (email !== '' || password !== '' || repeatPassword !== '') {
+        //     console.log('Campo(s) precisa(m) ser(em) preenchido(s)')   
+
+        //     if (password !== repeatPassword) {
+        //         console.log('Campos precisam ser iguais')   
+        //     } 
+
+
+        // }else {
+        //     console.log('Tudo ok com os dados')   
+        // }
+        
+        // else {
+        //     createUserWithEmailAndPassword(auth, email, password, repeatPassword)
+        //     .then((res) => {
+        //         console.log(res.user)
+        //       })
+        //     .catch(err => (err.message))
+        //     console.log('Usuário cadastrado com sucesso!')  
+        // }
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(email)
-        console.log(password)
-        console.log(repeatPassword)
+
+        handleValidatePassword()
     }
 
     return(
@@ -36,10 +67,6 @@ const FormSignup = () => {
                     <div className="mb-3">
                         <label htmlFor="inputPassword" className="form-label col-form-label-sm">Password</label>
                         <input type="password" className="form-control" id="inputPassword" onChange={handlePassword} />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="inputRepeatPassword" className="form-label col-form-label-sm">Repeat Password</label>
-                        <input type="password" className="form-control" id="inputRepeatPassword" onChange={handleRepeatPassword} />
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
